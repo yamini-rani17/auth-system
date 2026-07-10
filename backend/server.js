1eea667a05b13843d9cd298235c72a7e74474a56
Yamini Rani <yaminirani1999@gmail.com>
2026-07-10 20:53:34 +0530

    fix mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-  
-dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+connectDB(); // 🔥 important
 
-// ✅ Test route
 app.get("/", (req, res) => {
-  res.send("Backend Running 🚀");
+  res.send("API Running 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running");
 });
